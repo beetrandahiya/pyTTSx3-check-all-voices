c03d5611fe70fea046722b56e6e741c835d7b803
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2020-08-14 22:08:34 +0530

    Update tts-test.py

--- tts-test.py
+++ tts-test.py
@@ -1,10 +1,11 @@
 import pyttsx3
 engine = pyttsx3.init()
+test_txt=input("Enter the text you want to hear")
 voices = engine.getProperty('voices')
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate-30)                                                     #you can change the rate here
 for voice in voices:
     print(voice)
     engine.setProperty('voice',voice.id)
-    engine.say("Hello, This is the voice you'll hear if you choose me.")       #here goes the text you want to hear as a test
+    engine.say(test_txt)       
     engine.runAndWait()
